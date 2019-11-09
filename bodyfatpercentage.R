21a13b88e2331111fb365d94934462ce45f0b13e
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2019-11-08 21:29:12 -0800

    R Project Created

---
+++ bodyfatpercentage.R
@@ -0,0 +1,3 @@
+bodyfatpercentage <- function(Age, Weight, Height, Neck, Chest, Abdomen, Hip, Thigh, Knee, Ankle, Biceps, Forearm, Wrist) {
+  # your code goes here
+}
---
+++ full_code.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(magrittr)
+library(modelr)
+
+body_composition <- read.csv("http://staff.pubhealth.ku.dk/~tag/Teaching/share/data/Bodyfat.csv")
