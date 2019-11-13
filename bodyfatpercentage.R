aa637208269c44d423e3ef2cd45d565f7f61f8db
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2019-11-12 17:13:44 -0800

    Currently Looking For Formula for Body Density

--- bodyfatpercentage.R
+++ bodyfatpercentage.R
@@ -1,3 +1,21 @@
-bodyfatpercentage <- function(Age, Weight, Height, Neck, Chest, Abdomen, Hip, Thigh, Knee, Ankle, Biceps, Forearm, Wrist) {
+bodyfatpercentage <- function(Age, Chest, Abdomen, Thigh){
   # your code goes here
+  
+  #Link for Equations: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2891061/#FD1
+  
+  S <- (Chest + Abdomen + Thigh) / 10
+  
+  #a <- 1.10938
+  #b <- 0.0008267
+  #c <- 0.0000016
+  #d <- 0.000257
+  #BD <- round(a - b * S + c * S^2 - d * Age, digits=4)
+  
+  a <- 0.109648
+  b <- 0.0021745
+  k <- 0.747
+  d <- 0.0002516
+  BD <- round(exp(a - b * S^k - d * Age), digits=4)
+  
+  return(BD)
 }
--- full_code.R
+++ full_code.R
@@ -1,5 +1,20 @@
+install.packages("class")
 library(tidyverse)
 library(magrittr)
 library(modelr)
+library(class)
+source("bodyfatpercentage.R")
 
-body_composition <- read.csv("http://staff.pubhealth.ku.dk/~tag/Teaching/share/data/Bodyfat.csv")
+bodies <- read.csv("http://staff.pubhealth.ku.dk/~tag/Teaching/share/data/Bodyfat.csv")
+View(cor(bodies))
+
+for (i in 1:nrow(cor(bodies))){
+  correlations <-  which((cor(bodies)[i,] > 0.7) & (cor(bodies)[i,] != 1))
+  
+  if(length(correlations)> 0){
+    print(colnames(bodies)[i])
+    print(correlations)
+  }
+}
+
+View(bodies %>% mutate(BD = bodyfatpercentage(Age, Chest, Abdomen, Thigh)) %>% select(Density, BD))
