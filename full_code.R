0f6ca7ba4d4a04d06f093692293a906c8794ab6b
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2019-11-14 18:46:38 -0800

    Looking for Outliers to Remove

--- full_code.R
+++ full_code.R
@@ -6,12 +6,7 @@ library(class)
 source("bodyfatpercentage.R")
 
 bodies <- read.csv("http://staff.pubhealth.ku.dk/~tag/Teaching/share/data/Bodyfat.csv")
-bodies <- bodies %>% mutate(bodyfat = round(bodyfat, digits=0))
 
-#Identify candidate variables
-bodyfat_cor <- cor(bodies)[,2]
-bodyfat_cor <- bodyfat_cor[bodyfat_cor > 0 & bodyfat_cor < 1]
-bodyfat_cor <- sort(bodyfat_cor, decreasing=TRUE)
-
-bodyfat_model <- lm(bodyfat ~ Age + Weight + Height + Neck + Chest + Abdomen + Hip + Thigh + Knee + Ankle + Biceps + Forearm + Wrist, data = bodies)
-bodies <- bodies %>% mutate(calc_bodyfat = bodyfatpercentage(bodyfat_model)) %>% select(bodyfat, calc_bodyfat)
+#Remove Outliers
+bodies <- bodies %>% filter(round(bodyfat, digits=0) == round(495 / Density - 450, digits=0)) #Fat percentages that don't satisfy Siri's Equation
+summary(lm(Density ~ bodyfat, bodies))
