aa25b26eb28f399739be099e2e432650d335bb88
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2019-11-17 02:56:48 -0800

    Found Best Model to Predict Body Fat, Now Working on Analysis

--- bodyfatpercentage.R
+++ bodyfatpercentage.R
@@ -1,4 +1,10 @@
-bodyfatpercentage <- function(Model) {
+bodyfatpercentage <- function(Abdomen, Weight, Wrist, Forearm){
   # your code goes here
-  return(round(Model$fitted.values, digits=0))
+  bodyfat <- -36.6803
+  bodyfat <- bodyfat + 1.0156 * Abdomen
+  bodyfat <- bodyfat - 0.1395 * Weight
+  bodyfat <- bodyfat - 1.6907 * Wrist
+  bodyfat <- bodyfat + 0.6207 * Forearm
+  
+  return(round(bodyfat, digits=1))
 }
--- full_code.R
+++ full_code.R
@@ -1,42 +1,97 @@
-install.packages("class")
 library(tidyverse)
 library(magrittr)
 library(modelr)
-library(class)
 source("bodyfatpercentage.R")
 
 bodies <- read.csv("http://staff.pubhealth.ku.dk/~tag/Teaching/share/data/Bodyfat.csv")
 
-#Remove fat percentages that don't satisfy Siri's Equation
-bodies <- bodies %>% filter(bodyfat == round(495 / Density - 450, digits=1))
+#Remove fat percentages that don't satisfy Siri's Equation & the Density column
+bodies <- bodies %>% filter(bodyfat == round(495 / Density - 450, digits=1)) %>% select(-1)
 
-#Remove outliers
-is_outlier <- function(x){
-  return(x < quantile(x, 0.25) - 1.5 * IQR(x) | x > quantile(x, 0.75) + 1.5 * IQR(x))
-}
+#Pick the best predictors by creating a linear model using the step() function
+nullmodel <- lm(bodyfat ~ 1, bodies)
+fullmodel <- lm(bodyfat ~ ., bodies)
+model <- step(nullmodel, data = bodies, scope = list(upper = fullmodel, lower = nullmodel), direction = "both", k = 2, test = "F")
+summary(model)
 
-bodies <- bodies %>% mutate(outlier = is_outlier(Weight) | is_outlier(Height) | is_outlier(Neck) |
-                            is_outlier(Chest) | is_outlier(Abdomen) | is_outlier(Hip) | 
-                            is_outlier(Thigh) | is_outlier(Knee) | is_outlier(Ankle) | 
-                            is_outlier(Biceps) | is_outlier(Forearm) | is_outlier(Wrist)) %>% filter(outlier == FALSE)
+#Plot to check which powers of predictors are appropriate by checking if the relationships are linear
+bodyfat_abdomen <- lm(bodyfat ~ Abdomen, bodies)$coefficients
+ggplot(data = bodies) + geom_point(mapping = aes(x = Abdomen, y = bodyfat)) + geom_abline(slope = bodyfat_abdomen[2], intercept = bodyfat_abdomen[1])
 
-#Train 50% of the data
-trainindex <- seq(1, nrow(bodies), 2)
-trainfeatures <- bodies[trainindex, 3:15]
-trainlabels <- bodies[trainindex, 2]
+bodyfat_weight <- lm(bodyfat ~ Weight, bodies)$coefficients
+ggplot(data = bodies) + geom_point(mapping = aes(x = Weight, y = bodyfat)) + geom_abline(slope = bodyfat_weight[2], intercept = bodyfat_weight[1])
 
-#Test 50% of the data
-testindex <- seq(2, nrow(bodies), 2)
-testfeatures <- bodies[testindex, 3:15]
-testlabels <- bodies[testindex, 2]
+bodyfat_wrist <- lm(bodyfat ~ Wrist, bodies)$coefficients
+ggplot(data = bodies) + geom_point(mapping = aes(x = Wrist, y = bodyfat)) + geom_abline(slope = bodyfat_wrist[2], intercept = bodyfat_wrist[1])
 
-errorsVec <- vector("integer")
-for (k in seq(1, 15, 2))
+bodyfat_forearm <- lm(bodyfat ~ Forearm, bodies)$coefficients
+ggplot(data = bodies) + geom_point(mapping = aes(x = Forearm, y = bodyfat)) + geom_abline(slope = bodyfat_forearm[2], intercept = bodyfat_forearm[1])
+
+bodyfat_neck <- lm(bodyfat ~ Neck, bodies)$coefficients
+ggplot(data = bodies) + geom_point(mapping = aes(x = Neck, y = bodyfat)) + geom_abline(slope = bodyfat_neck[2], intercept = bodyfat_neck[1])
+
+bodyfat_age <- lm(bodyfat ~ Age, bodies)$coefficients
+ggplot(data = bodies) + geom_point(mapping = aes(x = Age, y = bodyfat)) + geom_abline(slope = bodyfat_age[2], intercept = bodyfat_age[1])
+
+bodyfat_thigh <- lm(bodyfat ~ Thigh, bodies)$coefficients
+ggplot(data = bodies) + geom_point(mapping = aes(x = Thigh, y = bodyfat)) + geom_abline(slope = bodyfat_thigh[2], intercept = bodyfat_thigh[1])
+
+#Significance & Partial F test
+modelA <- lm(bodyfat ~ Abdomen + Weight + Wrist + Forearm + Neck + Age, data = bodies) #Testing Thigh
+anova(modelA, model) #Remove Thigh (P-Value > 0.05)
+
+modelB <- lm(bodyfat ~ Abdomen + Weight + Wrist + Forearm + Neck, data = bodies) #Testing Age
+anova(modelB, modelA) #Remove Age (P-Value > 0.05)
+
+modelC <- lm(bodyfat ~ Abdomen + Weight + Wrist + Forearm, data = bodies) #Testing Neck
+anova(modelC, modelB) #Remove Neck (P-Value > 0.05)
+
+modelD <- lm(bodyfat ~ Abdomen + Weight + Wrist, data = bodies) #Testing Forearm
+anova(modelD, modelC) #Keep Forearm (P-Value < 0.05)
+
+modelE <- lm(bodyfat ~ Abdomen + Weight + Forearm, data = bodies) #Testing Wrist
+anova(modelE, modelC) #Keep Wrist (P-Value < 0.05)
+
+modelF <- lm(bodyfat ~ Abdomen + Wrist + Forearm, data = bodies) #Testing Weight
+anova(modelF, modelC) #Keep Weight (P-Value < 0.05)
+
+modelG <- lm(bodyfat ~ Weight + Wrist + Forearm, data = bodies) #Testing Abdomen
+anova(modelG, modelC) #Keep Abdomen (P-Value < 0.05)
+
+#Model C is the best linear model out of Models A to G
+redmodel <- modelC
+summary(redmodel)
+
+#Remove Outliers out of the data and the reduced model
+influencers <- influence.measures(redmodel)
+outliers <- data.matrix(which(apply(influencers$is.inf, 1, any)))
+for (i in 1:nrow(outliers))
 {
-  predictedlabels <- knn(train = trainfeatures, cl = trainlabels, test = testfeatures, k)
-  confusiontable <- table(testlabels, predictedlabels)
-  errorsVec <- c(errorsVec, sum(confusiontable) - sum(diag(confusiontable)))
+  bodies <- bodies[-outliers[i] + i - 1, ]
 }
+redmodel <- lm(bodyfat ~ Abdomen + Weight + Wrist + Forearm, data = bodies)
+summary(redmodel)
 
-k_vs_error <- data.frame(k=seq(1, 15, 2), errors=errorsVec)
-ggplot(data=k_vs_error) + geom_line(mapping = aes(x=k, y=errors))
+
+#Cross-Validation
+set.seed(2019)
+trainindex <- sample(1:nrow(bodies), 0.8 * nrow(bodies))
+trainset <- bodies[trainindex, ]
+testset <- bodies[-trainindex, ]
+
+trainmodel <- lm(bodyfat ~ Abdomen + Weight + Wrist + Forearm, data = trainset)
+summary(trainmodel)
+
+testframe <- data.frame(Abdomen = testset$Abdomen, 
+                        Weight = testset$Weight,
+                        Wrist = testset$Wrist,
+                        Forearm = testset$Forearm)
+
+#Get 95% confidence interval for the mean and value
+predictions_ci <- data.frame(predict(trainmodel, testframe, interval = "predict", level = 0.95))
+
+#Count number of observations that is within the value intervals and within 5 from predicted values
+nrow(testset[testset$bodyfat >= predictions_ci$lwr & testset$bodyfat <= predictions_ci$upr, ]) # 40/41
+nrow(testset[abs(testset$bodyfat - predictions_ci$fit) <= 5, ]) #31/41
+nrow(testset[testset$bodyfat - predictions_ci$fit < 0, ]) #20 / 41
+nrow(testset[testset$bodyfat - predictions_ci$fit > 0, ]) #21 / 41
